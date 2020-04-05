83d24cbb088615c5ad0208dbfd3941a8564fffc9
Andrew Weeks <aweeks@corelogic.com>
2020-04-04 21:00:10 -0700

    Go back to 1 migration script

--- src/main/resources/db/migration/V1__baselineTables.sql
+++ src/main/resources/db/migration/V1__baselineTables.sql
@@ -1,9 +1,45 @@
+create table game_room
+(
+  id                            bigserial    not null,
+    code varchar(6) unique  not null,
+    type varchar(25) not null,
+    name varchar(25),
+    round integer default 1 not null,
+primary key (id)
+);
+
 create table player
 (
   id                            bigserial    not null,
-    quizcode varchar(6) not null,
-    "name"  varchar(100) unique not null,
+    game_room_id bigint not null references game_room(id),
+    "name"  varchar(100) not null,
+    unique("name", game_room_id),
+    primary key (id)
+);
+
+create table answer
+(
+  id                            bigserial    not null,
+    answer varchar,
+    correct boolean,
 primary key (id)
 );
--- TODO UNIQUENESS SHOULD BE COMPOUND KEY
--- TODO quizcode should be own table?
+
+create table game_round
+(
+  id                            bigserial    not null,
+    round bigint not null,
+    player_id bigint references player(id) not null,
+    answer1_id bigint references answer(id),
+    answer2_id bigint references answer(id),
+    answer3_id bigint references answer(id),
+    answer4_id bigint references answer(id),
+    answer5_id bigint references answer(id),
+    answer6_id bigint references answer(id),
+    answer7_id bigint references answer(id),
+    answer8_id bigint references answer(id),
+    answer9_id bigint references answer(id),
+    answer10_id bigint references answer(id),
+    answer_theme_id bigint references answer(id),
+primary key (id)
+);
