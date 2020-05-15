8712e1b3601082329a918a34db8e7610c8d6103c
Andrew Weeks <aweeks@corelogic.com>
2020-05-15 10:47:46 -0700

    Alter character limit for answers from 50 to 300

---
+++ src/main/resources/db/migration/V2__alertImagineIfQuestionLiftCharLimit.sql
@@ -0,0 +1,7 @@
+ALTER TABLE imagine_if_question ALTER COLUMN answer1 TYPE varchar(300);
+ALTER TABLE imagine_if_question ALTER COLUMN answer2 TYPE varchar(300);
+ALTER TABLE imagine_if_question ALTER COLUMN answer3 TYPE varchar(300);
+ALTER TABLE imagine_if_question ALTER COLUMN answer4 TYPE varchar(300);
+ALTER TABLE imagine_if_question ALTER COLUMN answer5 TYPE varchar(300);
+ALTER TABLE imagine_if_question ALTER COLUMN answer6 TYPE varchar(300);
+
